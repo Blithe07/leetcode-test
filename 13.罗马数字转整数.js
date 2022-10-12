1203c8fb35ae22d9af69b4be45a922b6f7478d17
laitongbin <laitongbin@yhfin.com>
2022-10-12 14:59:22 +0800

    feat(leetcode): 13.罗马数字转整数

--- 13.罗马数字转整数.js
+++ 13.罗马数字转整数.js
@@ -10,24 +10,44 @@
  * @return {number}
  */
 var romanToInt = function (s) {
+    /**
+     * 通过从后往前读
+     let result = 0
+     // 已经读过的长度
+     let readLen = 0
+     // 从后往前读
+     let curWord = s[s.length - 1]
+     while (readLen !== s.length) {
+         // 前一个罗马字符
+         const prevWord = s[s.length - 2 - readLen]
+         let word = curWord
+         if ((curWord === 'V' && prevWord === 'I') || (curWord === 'X' && prevWord === 'I') || (curWord === 'L' && prevWord === 'X') || (curWord === 'C' && prevWord === 'X') || (curWord === 'D' && prevWord === 'C') || (curWord === 'M' && prevWord === 'C')) {
+             word = prevWord + word
+         }
+         const { result: r, readLen: rl } = changeData(word)
+         result += r
+         readLen += rl;
+         curWord = s[s.length - 1 - readLen]
+         continue
+     }
+     return result
+     */
+    /**
+     * 从前往后读，遇到下一位比当前位大的做减法，否则做加法
+     */
     let result = 0
-    // 已经读过的长度
-    let readLen = 0
-    // 从后往前读
-    let curWord = s[s.length - 1]
-    while (readLen !== s.length) {
-        // 前一个罗马字符
-        const prevWord = s[s.length - 2 - readLen]
-        let word = curWord
-        if ((curWord === 'V' && prevWord === 'I') || (curWord === 'X' && prevWord === 'I') || (curWord === 'L' && prevWord === 'X') || (curWord === 'C' && prevWord === 'X') || (curWord === 'D' && prevWord === 'C') || (curWord === 'M' && prevWord === 'C')) {
-            word = prevWord + word
+    let preNum = getValue(s[0])
+    for (let i = 1; i < s.length; i++) {
+        const curNum = getValue(s[i])
+        if (curNum > preNum) {
+            result -= preNum
+        } else {
+            result += preNum
         }
-        const { result: r, readLen: rl } = changeData(word)
-        result += r
-        readLen += rl;
-        curWord = s[s.length - 1 - readLen]
-        continue
+        preNum = curNum
     }
+    // 右值永远为正，因此最后一位必然为正
+    result += preNum
     return result
 };
 /**
@@ -122,5 +142,22 @@ const changeData = (value) => {
         default: break
     }
     return data
+}
+/**
+ * 返回罗马字符对应整数
+ * @param {string} ch 
+ * @returns 
+ */
+const getValue = (ch) => {
+    switch (ch) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
 }
 // @lc code=end
