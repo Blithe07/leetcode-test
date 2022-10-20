699b5e34a94a9c7d974f174e455fb95984be5d28
laitongbin <laitongbin@yhfin.com>
2022-10-20 11:02:16 +0800

    feat(leetcode): 94.二叉树的中序遍历

---
+++ 94.二叉树的中序遍历.js
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+    /**
+     * 递归，内部维护栈
+     const result = []
+     const inorder = (root) => {
+         if (!root) return
+         inorder(root.left)
+         result.push(root.val)
+         inorder(root.right)
+     }
+     inorder(root)
+     return result
+     */
+    /**
+     * 迭代，手动维护栈
+     const res = [];
+     const stk = [];
+     while (root || stk.length) {
+         while (root) {
+             stk.push(root);
+             root = root.left;
+         }
+         root = stk.pop();
+         res.push(root.val);
+         root = root.right;
+     }
+     return res;
+     */
+    /**
+     * morris遍历
+     */
+    if (!root) return []
+    const result = []
+    let cur = root, mostRight
+    while (cur) {
+        mostRight = cur.left
+        if (mostRight) {
+            while (mostRight.right && mostRight.right !== cur) {
+                mostRight = mostRight.right
+            }
+            if (!mostRight.right) {
+                mostRight.right = cur
+                cur = cur.left
+                continue
+            } else {
+                mostRight = null
+            }
+        }
+        // 没有左树或者第二次遇到同一节点(对应1、2)
+        result.push(cur.val);
+        cur = cur.right
+    }
+    return result
+
+};
+// @lc code=end
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
