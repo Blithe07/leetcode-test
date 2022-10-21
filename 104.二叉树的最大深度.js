b9e1676a4c4197fad7589f0ed0be356fe1f1bca6
laitongbin <laitongbin@yhfin.com>
2022-10-21 16:59:11 +0800

    feat(leetcode): 104.二叉树的最大深度

--- 104.二叉树的最大深度.js
+++ 104.二叉树的最大深度.js
@@ -18,7 +18,13 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-
+    if (root === null) return 0
+    return Math.max(getDeep(root.left), getDeep(root.right)) + 1
 };
 // @lc code=end
 
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
