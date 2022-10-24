cde03463809e9588aa1b94e99f150b6d7c51e17a
laitongbin <laitongbin@yhfin.com>
2022-10-24 10:08:42 +0800

    feat(leetcode): 67.二进制求和

--- 67.二进制求和.js
+++ 67.二进制求和.js
@@ -16,10 +16,10 @@ var addBinary = function (a, b) {
     let ca = 0;
     for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {
         let sum = ca;
-        sum += i >= 0 ? parseInt(a[i]) : 0;
-        sum += j >= 0 ? parseInt(b[j]) : 0;
+        sum += i >= 0 ? +a[i] : 0;
+        sum += j >= 0 ? +b[j] : 0;
         ans += sum % 2;
-        ca = Math.floor(sum / 2);
+        ca = sum >> 1
     }
     ans += ca == 1 ? ca : "";
     // 得到的ans是反向的字符串
