e4c39fb5ddf98b992eb01f8ef94865271f263fcd
laitongbin <laitongbin@yhfin.com>
2022-10-25 16:04:29 +0800

    feat(leetcode): 168.excel表列名称

--- 168.excel表列名称.js
+++ 168.excel表列名称.js
@@ -9,8 +9,15 @@
  * @param {number} columnNumber
  * @return {string}
  */
-var convertToTitle = function(columnNumber) {
-
+var convertToTitle = function (columnNumber) {
+    let ans = []
+    while (columnNumber > 0) {
+        // 避免老六字母Z会转换成@
+        const a0 = (columnNumber - 1) % 26 + 1
+        ans.push(String.fromCharCode(a0 + 'A'.charCodeAt() - 1))
+        columnNumber = Math.floor((columnNumber - a0) / 26)
+    }
+    return ans.reverse().join('')
 };
 // @lc code=end
 
