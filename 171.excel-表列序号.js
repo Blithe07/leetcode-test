17aa461c7d9e3c4cd0bab683c1e467d94ab9c61a
laitongbin <laitongbin@yhfin.com>
2022-10-25 15:28:44 +0800

    feat(leetcode): 171.excel-表列序号

---
+++ 171.excel-表列序号.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=javascript
+ *
+ * [171] Excel 表列序号
+ */
+
+// @lc code=start
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function (columnTitle) {
+    let number = 0
+    // 累加指数结果
+    let multi = 1
+    for (let i = columnTitle.length - 1; i >= 0; i--) {
+        const k = columnTitle[i].charCodeAt() - 'A'.charCodeAt() + 1
+        number += k * multi
+        multi *= 26
+    }
+    return number
+};
+// @lc code=end
+
