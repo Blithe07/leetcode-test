68b0e0bbc0f58ccba8e87384a57b3f22e8101905
laitongbin <laitongbin@yhfin.com>
2022-10-25 13:32:17 +0800

    feat(leetcode): 160.相交链表

---
+++ 160.相交链表.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+    /** A先走完自己的路，再走B的路；B走完自己的路再走A的路，一定相遇。要么有结果，要么都是null */
+    let pA = headA, pB = headB;
+    while (pA !== pB) {
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
+    }
+    return pA;
+};
+// @lc code=end
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
