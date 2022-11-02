ff6b7150e2d3563a3bbb0d4ce5165a428e4be081
laitongbin <laitongbin@yhfin.com>
2022-11-02 10:31:50 +0800

    feat(leetcode): 226.翻转二叉树

---
+++ 226.翻转二叉树.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (!root) return root
+    invertTree(root.left)
+    invertTree(root.right)
+    const tmp = root.left
+    root.left = root.right
+    root.right = tmp
+    return root
+};
+// @lc code=end
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
