4d98a1d537b416b02a26b8fcd5913958e1ee2844
laitongbin <laitongbin@yhfin.com>
2022-11-03 14:03:26 +0800

    feat(leetcode): 234.回文链表

--- 234.回文链表.js
+++ 234.回文链表.js
@@ -17,6 +17,48 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
+    /**
+     * 时间O(N),空间O(N)
+     */
+    // const vals = []
+    // while (head) {
+    //     vals.push(head.value)
+    //     head = head.next
+    // }
+    // for (let i = 0, j = vals.length - 1; i < j; i++, j--) {
+    //     if (vals[i] !== vals[j]) {
+    //         return false
+    //     }
+    // }
+    // return true
+    /**
+     * 时间O(N),空间O(N/2)
+     */
+    // if (!head || !head.next) return true
+    // // 少比较一次
+    // let right = head.next
+    // let cur = head
+    // // 快指针走两步，慢指针走一步
+    // while (cur.next && cur.next.next) {
+    //     right = right.next
+    //     cur = cur.next.next
+    // }
+    // const stack = new Array()
+    // // 到中点后，数据压入栈中
+    // while (right) {
+    //     stack.push(right.value)
+    //     right = right.next
+    // }
+    // while (stack.length) {
+    //     // 弹出比较，不一致则不是回文
+    //     if (head.value !== stack.pop()) return false
+    //     // 一致继续比较
+    //     else head = head.next
+    // }
+    // return true
+    /**
+     * 时间O(N),空间O(1)
+     */
     let slow = head, fast = head
     // slow走到中点
     while (fast.next && fast.next.next) {
