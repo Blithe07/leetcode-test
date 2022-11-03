36cff650a76fc9ff172eee4f1971b166cd087a96
laitongbin <laitongbin@yhfin.com>
2022-11-03 14:53:04 +0800

    feat(leetcode): 242.有效的字母异位词

--- 242.有效的字母异位词.js
+++ 242.有效的字母异位词.js
@@ -31,5 +31,3 @@ var isAnagram = function (s, t) {
     // return s.length === t.length && [...s].sort().join('') === [...t].sort().join('')
 };
 // @lc code=end
-
-console.log(isAnagram('ret', 'car'));
