f079dcccf51acbd01c171db7db86af6b869fc92c
Blithe07 <65998645+Blithe07@users.noreply.github.com>
2022-11-16 14:49:01 +0800

    feat(leetcode): 412.fizz-buzz

---
+++ 412.fizz-buzz.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=412 lang=javascript
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+    const result = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0) {
+            if (i % 5 == 0) {
+                result.push('FizzBuzz')
+            } else {
+                result.push('Fizz')
+            }
+        } else if (i % 5 === 0) {
+            result.push('Buzz')
+
+        } else {
+            result.push(i)
+        }
+    }
+    return result
+};
+// @lc code=end
+
