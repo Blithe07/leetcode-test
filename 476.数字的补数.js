6c760fcabfcec14adabe1111acc9d5eaff2e852d
Blithe07 <65998645+Blithe07@users.noreply.github.com>
2022-11-25 12:11:58 +0800

    feat(leetcode): 476.数字的补数

---
+++ 476.数字的补数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=476 lang=javascript
+ *
+ * [476] 数字的补数
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function (num) {
+    return num ^ parseInt('1'.repeat(num.toString('2').length), 2)
+};
+// @lc code=end
+
