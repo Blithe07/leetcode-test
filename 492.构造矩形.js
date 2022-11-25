956a5d704c50215f91f2e1bb6a46c08a4ac31ed3
Blithe07 <65998645+Blithe07@users.noreply.github.com>
2022-11-25 15:39:01 +0800

    feat(leetcode): 492.构造矩形

---
+++ 492.构造矩形.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=492 lang=javascript
+ *
+ * [492] 构造矩形
+ */
+
+// @lc code=start
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function (area) {
+    let W = Math.floor(Math.sqrt(area))
+    while (area % W !== 0) {
+        W--
+    }
+    return [area / W, W]
+};
+// @lc code=end
+
