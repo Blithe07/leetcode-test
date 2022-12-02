a9ba2bc904cf96e021a8ce9b5ec1566382b10783
Blithe07 <65998645+Blithe07@users.noreply.github.com>
2022-12-02 13:27:34 +0800

    feat(leetcode): 605.种花问题

---
+++ 605.种花问题.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=605 lang=javascript
+ *
+ * [605] 种花问题
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] == 0 && (i + 1 == flowerbed.length || flowerbed[i + 1] == 0)) {
+            n--;
+            i++;
+        } else if (flowerbed[i] == 1) {
+            i++;
+        }
+    }
+    return n <= 0
+};
+// @lc code=end
+
