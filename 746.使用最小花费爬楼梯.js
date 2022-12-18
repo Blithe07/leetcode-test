94df20e434d0066b378261d9eb3884b724f49f40
Blithe07 <65998645+Blithe07@users.noreply.github.com>
2022-12-18 21:57:43 +0800

    feat(leetcode): 746.使用最小花费爬楼梯

--- 746.使用最小花费爬楼梯.js
+++ 746.使用最小花费爬楼梯.js
@@ -12,7 +12,7 @@
 var minCostClimbingStairs = function (cost) {
     let prev = 0, cur = 0;
     const n = cost.length
-    for (let i = 2; i < n; i++) {
+    for (let i = 2; i <= n; i++) {
         const next = Math.min(cur + cost[i - 1], prev + cost[i - 2])
         prev = cur
         cur = next
