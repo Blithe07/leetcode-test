ddce125e2d41f0d4b76aba4e15cc41c76a0f813d
Blithe07 <65998645+Blithe07@users.noreply.github.com>
2022-12-18 19:57:04 +0800

    feat(leetcode): 744.寻找比目标字母大的最小字母

---
+++ 744.寻找比目标字母大的最小字母.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=744 lang=javascript
+ *
+ * [744] 寻找比目标字母大的最小字母
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function (letters, target) {
+    /**
+     * 线性查找，时间复杂度O(N)，是、空间复杂度O(1)
+     */
+    // for (let i = 0; i < letters.length; i++) {
+    //     if (letters[i] > target) {
+    //         return i
+    //     }
+    // }
+    // return letters[0]
+    /**
+     * 二分查找，时间复杂度O(logN)，是、空间复杂度O(1)
+     */
+    if (target >= letters[letters.length - 1]) return letters[0]
+    let left = 0, right = letters.length - 1
+    while (left < right) {
+        const mid = (left + right) >> 1
+        if (letters[mid] > target) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return letters[left]
+};
+// @lc code=end
+
