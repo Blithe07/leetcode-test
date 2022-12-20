94cdb2792a6fb6ae4652f179b8dc038f5ea9f4bf
Blithe07 <65998645+Blithe07@users.noreply.github.com>
2022-12-20 10:15:04 +0800

    feat(leetcode): 27.移除元素

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -11,14 +11,31 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    const n = nums.length;
-    let left = 0;
-    for (let right = 0; right < n; right++) {
-        if (nums[right] !== val) {
-            nums[left] = nums[right];
-            left++;
+    /**
+     * 等于val的往后推，最差例子：[1,2,3,4,5] val=1
+     */
+    // const n = nums.length;
+    // let left = 0;
+    // for (let right = 0; right < n; right++) {
+    //     if (nums[right] !== val) {
+    //         nums[left] = nums[right];
+    //         left++;
+    //     }
+    // }
+    // return left;
+    /**
+     * 头尾指针，避免最坏例子
+     */
+    const n = nums.length
+    let left = 0, right = n
+    while (left < right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right - 1]
+            right--
+        } else {
+            left++
         }
     }
-    return left;
+    return left
 };
 // @lc code=end
